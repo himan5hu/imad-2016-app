fa1a88a7ba8461d22dc245b6f6502cdfaafcb634
himan5hu <himanshuginkal_mohan@srmuniv.edu.in>
2016-10-13 17:21:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article one | Himanshu Ginkal',
+    heading: 'Article one',
+    date: 'October 13, 2016',
+    content: `<p>
+                This is the content for the Article one on my webapp! Nice to see this is good going uptill now. Thanks Tanmai for doing this! This is helping a lot of people understand the concepts and how web applications work! Hopefully all of us make our own webApp in future and then surely remember IMAD-Team for this!
+                Cheers to this!
+            </p>
+            <p>
+                This is the content for the Article one on my webapp! Nice to see this is good going uptill now. Thanks Tanmai for doing this! This is helping a lot of people understand the concepts and how web applications work! Hopefully all of us make our own webApp in future and then surely remember IMAD-Team for this!
+                Cheers to this!
+            </p>
+            <p>
+                This is the content for the Article one on my webapp! Nice to see this is good going uptill now. Thanks Tanmai for doing this! This is helping a lot of people understand the concepts and how web applications work! Hopefully all of us make our own webApp in future and then surely remember IMAD-Team for this!
+                Cheers to this!
+            </p>`
+};
+
+function createTemplate(data){
+        var title = data.title, heading = data.heading, date=data.date, content=data.content;
+        var htmlTemplate = `
+                <html>
+                <head>
+                    <title>
+                        ${title}
+                    </title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    <link href="/ui/style.css" rel="stylesheet" />
+                </head>
+                <body>
+                    <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                        <hr/>
+                        <h3>${heading}</h3>
+                    </div>
+                    <div>
+                        ${date}
+                    </div>
+                    <div>
+                    ${content}
+                    </div>
+                    <h4>Thank You for all of this! </h4>
+                    You may not be seeing this but still
+                    </div>
+                </body>
+                </html>
+        `;
+        return htmlTemplate
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 app.get('/article-one', function(req, res)
 {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+   res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function(req, res)
 {
