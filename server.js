cf3470cb44f2ddce4739d86b5b6e7fc413ac212a
himan5hu <himanshuginkal_mohan@srmuniv.edu.in>
2016-10-13 19:19:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/:articleName', function(req, res)
    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/styles.css', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
