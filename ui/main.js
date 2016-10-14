f5c017df735422efa9d7cb3b06c6f139d54a60e2
himan5hu <himanshuginkal_mohan@srmuniv.edu.in>
2016-10-14 20:46:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 console.log('Loaded!');
 //Change text of mainText division
 //Move me
-var img = document.getElementById('me');
-var marginLeft=0;
-function moveRight() {
+//var img = document.getElementById('me');
+//var marginLeft=0;
+//function moveRight() {
      marginLeft = marginLeft+1;
      img.style.marginLeft = marginLeft+'px';
 }
-function moveLeftt() {
+//function moveLeftt() {
      marginLeft = marginLeft-10;
      img.style.marginLeft = marginLeft+'px';
 }
-img.onclick = function() {
-    var intervalR = setInterval(moveRight, 10);
-    var intervalL = setInterval(moveLeft, 200);
-};
+//img.onclick = function() {
+//    var intervalR = setInterval(moveRight, 10);
+ //   var intervalL = setInterval(moveLeft, 200);
+//};
