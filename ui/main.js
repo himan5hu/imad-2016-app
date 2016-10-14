d2ce4fb6e0ae866a651c0ecf69a939c7ee723ec5
himan5hu <himanshuginkal_mohan@srmuniv.edu.in>
2016-10-14 21:07:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,17 @@ console.log('Loaded!');
 //    var intervalR = setInterval(moveRight, 10);
  //   var intervalL = setInterval(moveLeft, 200);
 //};
+
+// counter code
+var button = document.getElementById('counter');
+button.onclick = function() {
+    //Make a request
+    
+    //Capture the response and store in a variable
+    
+    //Render the variable in the right span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
