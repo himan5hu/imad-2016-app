b27e288aa49c82564e5e4d81aba42084ffbf5dca
himan5hu <himanshuginkal_mohan@srmuniv.edu.in>
2016-10-14 17:57:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //Change text of mainText division
 var element = document.getElementById('mainText');
 element.innerHTML = `New Value!`;
+
+//Move madi
+var img = document.getElementById('madi');
+img.onclick = function() {
+  img.style.marginLeft = '100px';
+    
+};
